ba0542d0a42617476bf455d02f7b5fdfe814d42d
Tekdino <tekdino@gmail.com>
2013-09-11 23:38:33 +0300

    ValtioLuettelo alkaa

---
+++ LastConflicts/src/ihmiset/ValtioLuettelo.java
@@ -0,0 +1,12 @@
+package ihmiset;
+
+/**
+ * Taulukko joka sisältää kaikki valtiot. Luokka sisältää myös työkaluja valtioiden
+ * listaamiseen, järjestämiseen jne. Ehkä myös uusien valtioiden luontia varten?
+ */
+public class ValtioLuettelo {
+    public Valtio[] valtioLista;
+    
+    public ValtioLuettelo() {
+    }
+}
